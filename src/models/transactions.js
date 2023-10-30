7f651d195628413f18861370294d0aee91da4cb6
Diego López Ariza <diegoawesome2017@outlook.com>
2023-10-29 18:50:33 -0600

    updated JSdoc

--- src/models/transactions.js
+++ src/models/transactions.js
@@ -1,22 +1,69 @@
 import { pipeline } from "@/validate/server"
-import { ObjectId } from "mongodb"
-
+import { Collection, ObjectId } from "mongodb"
+/**
+ * Create an Array of objects with delete operations for mongo bulkWrite
+ * @param {String[]} _ids Array of _ids of the documents to delete
+ * @returns Array of objects with delete operations
+ */
 const deleteOperations = _ids => _ids.map(_id => ({ deleteOne: { filter: { _id: new ObjectId(_id) } } }))
-
+/**
+ * Find a document by an _id
+ * @param {Collection} collection Mongo collection to work with
+ * @param {String} _id The document _id to find
+ * @returns 
+ */
 export const readItem = (collection, _id) => collection.find({ _id: new ObjectId(_id) }).toArray()
-
+/**
+ * Retrieve the documents in a collection
+ * @param {Collection} collection Mongodb collection to work with
+ * @param {Object} filter the search finter
+ * @returns 
+ */
 export const readItems = (collection, filter = {}) => collection.find(filter).toArray()
-
+/**
+ * Insert a document in a collection
+ * @param {Collection} collection Mongo collection to work with
+ * @param {Object} item item to write in the collection
+ * @returns 
+ */
 export const createItem = (collection, item) => collection.insertOne(item)
-
+/**
+ * Insert many documents in a database
+ * @param {Collection} collection Mongo collection to work with
+ * @param {Object[]} items items to write in the database
+ * @returns 
+ */
 export const createItems = (collection, items) => collection.insertMany(items)
-
+/**
+ * Delete a document
+ * @param {Collection} collection Mongo collection to work with
+ * @param {String} _id _id of the document to delete
+ * @returns 
+ */
 export const deleteItem = (collection, _id) => collection.deleteOne({ _id: new ObjectId(_id) })
-
+/**
+ * Delete documents by passing an array of _ids
+ * @param {Collection} collection Mongo collection to work with
+ * @param {String[]} _ids Array of _ids in string format
+ * @returns 
+ */
 export const deleteItems = (collection, _ids) => collection.bulkWrite(deleteOperations(_ids))
-
+/**
+ * Updates a document
+ * @param {Collection} collection Mongo collection to work with
+ * @param {String} _id _id of the document to update
+ * @param {Object} item new document body
+ * @returns 
+ */
 export const updateItem = (collection, _id, item) => collection.updateOne({ _id: new ObjectId(_id) }, item)
-
+/**
+ * Handles the request of an API route
+ * @param {import("next").NextApiRequest} req request object
+ * @param {import("next").NextApiResponse} res response object
+ * @param {Collection} collection 
+ * @param {Function[]} validations 
+ * @returns 
+ */
 export const baseHandler = async (req, res, collection, validations) => {
     const { body, method } = req
     try {
@@ -40,7 +87,14 @@ export const baseHandler = async (req, res, collection, validations) => {
         return res.status(500).json({ error: { message: error.message } })
     }
 }
-
+/**
+ * Handles the request of a dynamic API route
+ * @param {import("next").NextApiRequest} req request object
+ * @param {import("next").NextApiResponse} res response object
+ * @param {Collection} collection 
+ * @param {Function[]} validations 
+ * @returns 
+ */
 export const dynamicHandler = async (req, res, collection, validations) => {
     const { body, method, query: { id } } = req
     if (!pipeline(validations[method], body)) {
